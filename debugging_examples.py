c2701798901a5e5b0c81f6e0c87a38e5a45909c6
user <user@users-MacBook-Pro.local>
2022-02-23 22:20:43 +0000

    Debugging example

---
+++ debugging_examples.py
@@ -0,0 +1,26 @@
+my_string = "This is a test string"
+
+length = len(my_string)
+
+print(length)
+
+
+
+my_list = [1, 2, 3, 4]
+
+print([i+4 for i in my_list])
+
+
+def add(a, b):
+  return a + b
+
+add(4, 8)
+
+
+
+my_dict = {
+  "name": "Beth",
+  "age": 25
+}
+
+numbers = [1, 2, 3]
